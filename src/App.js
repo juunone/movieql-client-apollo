5275369a2a1fd76d1e731a0ca2d3c2e255e61a4a
juunone <juunone@gmail.com>
2019-02-12 23:54:29 +0900

    home detail router config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route} from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo.js"
+import Home from './Home';
+import Detail from './Detail';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" />
+      <ApolloProvider client={client}>
+        <Router>
+          <React.Fragment>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route exact={true} path={"/details/:movie"} component={Detail} />
+          </React.Fragment>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
