d14eae6fbfbcf0d934e29168f9a63de3b9bc3f34
juunone <juunone@gmail.com>
2019-02-13 16:25:44 +0900

    movie ql with api setup

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import {Query} from 'react-apollo';
+import {HOME_PAGE} from './queries';
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+    <Query query={HOME_PAGE}>
+        {({loading, data, error})=>{
+            if(loading) return <span>loading</span>;
+            if(error) return <span>something error</span>;
+            return data.movies.map((movie)=>   
+                <h2 key={movie.id}>
+                    {movie.title} / {movie.rating}
+                </h2>
+            );
+        }}
+    </Query>
+);
+
+
 
 export default Home;
--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri:""
+    uri:"https://movieql.now.sh/"
 })
 
 export default client;
